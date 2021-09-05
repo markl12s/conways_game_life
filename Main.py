dacedcae4c906808f6cb9dd89de9903e57b78cb4
Mark <72723296+markl12s@users.noreply.github.com>
2021-09-05 11:19:43 -0700

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,110 +1,162 @@
-board = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+"""
+Conway's game of life, Mark Leard
+v 1.1.0
+
+last change: finished the main functionality
+
+things to do
+create check cell functions for sides and corners
+start to figure out how the led's will work
+find a cool pattern that will work for the final version/as decoration
+
+next task: debugging
+"""
+
+#board initial state
+board = [[0, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+         [1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
          [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+board_swap = board
 
+#functions/architecture
 def check_cell(target_cell_x, target_cell_y):
-    live_near = 0
+    live_cells_nearby = 0
 
-    try:
-        if board[target_cell_x - 1][target_cell_y - 1] == 1:
-            live_near += 1
-        if board[target_cell_x - 1][target_cell_y] == 1:
-            live_near += 1
-        if board[target_cell_x - 1][target_cell_y + 1] == 1:
-            live_near += 1
+    #corners
+    if target_cell_x == 0 and target_cell_y == 0:
+        live_cells_nearby = top_left_corner()
+    elif target_cell_x == -1 and target_cell_y == 0:
+        live_cells_nearby = bot_left_corner()
+    elif target_cell_x == 0 and target_cell_y == -1:
+        top_right_corner()
+    elif target_cell_x == -1 and target_cell_y == -1:
+        bot_right_corner()
 
-        if board[target_cell_x][target_cell_y - 1] == 1:
-            live_near += 1
-        if board[target_cell_x][target_cell_y] == 1:
-            live_near += 1
-        if board[target_cell_x][target_cell_y + 1] == 1:
-            live_near += 1
+    #middle cells
+    elif target_cell_x != 0 and target_cell_x != -1:
+        if target_cell_y != 0 and target_cell_y != -1:
+            live_cells_nearby = add_row(target_cell_x - 1, target_cell_y)
+            live_cells_nearby += add_sides(target_cell_x, target_cell_y)
+            live_cells_nearby += add_row(target_cell_x + 1, target_cell_y)
 
-        if board[target_cell_x + 1][target_cell_y - 1] == 1:
-            live_near += 1
-        if board[target_cell_x + 1][target_cell_y] == 1:
-            live_near += 1
-        if board[target_cell_x + 1][target_cell_y + 1] == 1:
-            live_near += 1
-
-    #edge-cases
-    except:
-        #corners
-        if target_cell_x == 0 and target_cell_y == 0:
-            #checks
-        elif target_cell_x == 0 and target_cell_y == -1:
-            #checks
-        elif target_cell_x == -1 and target_cell_y == 0:
-            #checks
-        elif target_cell_x == -1 and target_cell_y == -1:
-            #checks
-
-        #x is on top
-        elif target_cell_x == 0:
-            if target_cell_y != 0 or target_cell_y != -1:
-                if board[target_cell_x][target_cell_y - 1] == 1:
-                    live_near += 1
-                if board[target_cell_x][target_cell_y] == 1:
-                    live_near += 1
-                if board[target_cell_x][target_cell_y + 1] == 1:
-                    live_near += 1
-
-                if board[target_cell_x + 1][target_cell_y - 1] == 1:
-                    live_near += 1
-                if board[target_cell_x + 1][target_cell_y] == 1:
-                    live_near += 1
-                if board[target_cell_x + 1][target_cell_y + 1] == 1:
-                    live_near += 1
-
-        #x is on bottom
-        elif target_cell_x == -1:
-            if target_cell_y != 0 or target_cell_y != -1:
-                if board[target_cell_x - 1][target_cell_y - 1] == 1:
-                    live_near += 1
-                if board[target_cell_x - 1][target_cell_y] == 1:
-                    live_near += 1
-                if board[target_cell_x - 1][target_cell_y + 1] == 1:
-                    live_near += 1
-
-                if board[target_cell_x][target_cell_y - 1] == 1:
-                    live_near += 1
-                if board[target_cell_x][target_cell_y] == 1:
-                    live_near += 1
-                if board[target_cell_x][target_cell_y + 1] == 1:
-                    live_near += 1
-
-        #y is on left
-        elif target_cell_y == 0:
-            if target_cell_x != 0 or target_cell_x != -1:
-
-        #y is on bottom
-
-
-    finally:
-        if board[target_cell_x][target_cell_y] == 0:
-            if live_near == 3:
-                live_or_dead.append(1)
-            else:
-                live_or_dead.append(0)
-        elif board[target_cell_x][target_cell_y] == 1:
-            if live_near == 2:
-                live_or_dead.append(1)
-            elif live_near == 3:
-                live_or_dead.append(1)
-            else:
-                live_or_dead.append(0)
+    #check if cell is alive
+    if board[target_cell_x][target_cell_y] == 0:
+        if live_cells_nearby == 3:
+            return 1
+        else:
+            return 0
+    else:
+        if live_cells_nearby == 2:
+            return 1
+        elif live_cells_nearby == 3:
+            return 1
+        else:
+            return 0
 
 def turn_check():
     live_or_dead = []
     for x in range(10):
         for y in range(10):
-            check_cell(target_cell_x, target_cell_y)
+            dead_or_alive = check_cell(x, y)
+            board_swap[x][y] = dead_or_alive
 
-def change_states(target):
+#checking cells nearby
+def add_row(middle_x, middle_y):
+    row = []
+
+    row.append(board[middle_x][middle_y - 1])
+    row.append(board[middle_x][middle_y])
+    row.append(board[middle_x][(middle_y + 1) % 10])
+
+    return sum(row)
+
+def add_sides(middle_x, middle_y):
+    row = []
+
+    row.append(board[middle_x][middle_y - 1])
+    row.append(board[middle_x][(middle_y + 1) % 10])
+
+    return sum(row)
+
+#corners
+def top_left_corner():
+    nearby_cells = []
+
+    nearby_cells.append(board[-1][-1])
+    nearby_cells.append(board[-1][0])
+    nearby_cells.append(board[-1][1])
+
+    nearby_cells.append(board[0][-1])
+    nearby_cells.append(board[0][1])
+
+    nearby_cells.append(board[1][-1])
+    nearby_cells.append(board[1][0])
+    nearby_cells.append(board[1][1])
+
+    return sum(nearby_cells)
+
+def bot_left_corner():
+    nearby_cells = []
+
+    nearby_cells.append(board[-2][-1])
+    nearby_cells.append(board[-2][0])
+    nearby_cells.append(board[-2][1])
+
+    nearby_cells.append(board[-1][-1])
+    nearby_cells.append(board[-1][1])
+
+    nearby_cells.append(board[0][-1])
+    nearby_cells.append(board[0][0])
+    nearby_cells.append(board[0][1])
+
+    return sum(nearby_cells)
+
+def top_right_corner():
+    nearby_cells = []
+
+    nearby_cells.append(board[-1][0])
+    nearby_cells.append(board[0][0])
+    nearby_cells.append(board[1][0])
+
+    nearby_cells.append(board[1][-1])
+    nearby_cells.append(board[-1][-1])
+
+    nearby_cells.append(board[-1][-2])
+    nearby_cells.append(board[0][-2])
+    nearby_cells.append(board[1][-2])
+
+    return sum(nearby_cells)
+
+def bot_right_corner():
+    nearby_cells = []
+
+    nearby_cells.append(board[-1][0])
+    nearby_cells.append(board[0][0])
+    nearby_cells.append(board[1][0])
+
+    nearby_cells.append(board[-1][-1])
+    nearby_cells.append(board[1][-1])
+
+    nearby_cells.append(board[-1][-2])
+    nearby_cells.append(board[0][-2])
+    nearby_cells.append(board[1][-2])
+
+    return sum(nearby_cells)
+
+#execution
+print(board)
+turn_check()
+board = board_swap
+print(board)
+
+"""while True:
+    turn_check()
+    board = board_swap
+    print(board)"""
